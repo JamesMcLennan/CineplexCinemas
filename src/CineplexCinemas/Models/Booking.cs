03db295f4d3f69bdbd74b4d8e7975088b770263d
cre8iveallen <pyeinkiller@gmail.com>
2017-04-23 00:03:22 +1000

    minor changes in validation stuff

--- src/CineplexCinemas/Models/Booking.cs
+++ src/CineplexCinemas/Models/Booking.cs
@@ -9,14 +9,30 @@ namespace CineplexCinemas.Models
     public class Booking
     {
         [RegularExpression("^([a-zA-z,.-]+)$")Required(ErrorMessage = "Invalid Name")]
-        public string firstName { get; set; }
+        public string Firstname { get; set; }
+
         [RegularExpression("^([a-zA-z,.-]+)$")Required(ErrorMessage = "Invalid Name")]
-        public string lastName { get; set; }
+        public string Lastname { get; set; }
+
         [RegularExpression("^([a-zA-z0-9 ,.-]+)$")Required(ErrorMessage = "Invalid Address")]
-        public string address { get; set; }
-        [RegularExpression("^([a-z0-9_.-]+[@]{1}[a-z0-9]{1,15}[.]{1}[a-z]{2,3}[.]{0,1}[a-z]{0,3})$")Required(ErrorMessage = "Invalid Email")]
-        public string email { get; set; }
+        public string Unit { get; set; }
+
+        [RegularExpression("^([a-zA-z0-9 ,.-]+)$")Required(ErrorMessage = "Invalid Address")]
+        public string Street { get; set; }
+
+        [RegularExpression("^([a-zA-z0-9 ,.-]+)$")Required(ErrorMessage = "Invalid Address")]
+        public string Suburb { get; set; }
+
+        [RegularExpression("^([a-zA-z0-9 ,.-]+)$")Required(ErrorMessage = "Invalid Address")]
+        public string State { get; set; }
+
+        [RegularExpression("^([0-9]{4})$")Required(ErrorMessage = "Invalid Address")]
+        public string Postcode { get; set; }
+
+        [RegularExpression("^([a-z0-9_.-]+[@]{1}[a-z0-9]{1,15}[.]{1}[a-z]{1,5}[.]{0,1}[a-z]{0,5}[.]{0,1}[a-z]{0,5})$")Required(ErrorMessage = "Invalid Email")]
+        public string Email { get; set; }
+
         [RegularExpression("^(([+]?)([0-9]{3,4})([- ]?)([0-9]{3,4})([- ]?)([0-9]{3,4}))$")Required(ErrorMessage = "Invalid Phone")]
-        public string phoneNo { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
