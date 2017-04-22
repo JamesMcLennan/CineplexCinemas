64ca70577cf876ccc9198451aa0a20dce067e557
JamesMcLennan <jamesamclennan@gmail.com>
2017-04-23 08:15:09 +1000

    Create Events page in Enquiries. Added contact form partial

--- src/CineplexCinemas/Controllers/EnquiriesController.cs
+++ src/CineplexCinemas/Controllers/EnquiriesController.cs
@@ -24,6 +24,11 @@ namespace CineplexCinemas.Controllers
             return View(await _context.Enquiry.ToListAsync());
         }
 
+        public IActionResult Events()
+        {
+            return View();
+        }
+
         // GET: Enquiries/Details/5
         public async Task<IActionResult> Details(int? id)
         {
