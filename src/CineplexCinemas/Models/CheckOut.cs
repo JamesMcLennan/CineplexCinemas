475365b186e7f97d72a839384c32a16492ed20e6
cre8iveallen <pyeinkiller@gmail.com>
2017-04-23 08:41:11 +1000

    initial addition
    need to work

--- src/CineplexCinemas/Models/CheckOut.cs
+++ src/CineplexCinemas/Models/CheckOut.cs
@@ -9,25 +9,7 @@ namespace CineplexCinemas.Models
     public class CheckOut
     {
         [RegularExpression("^([a-zA-z,.-]+)$")Required(ErrorMessage = "Invalid Name")]
-        public string Firstname { get; set; }
-
-        [RegularExpression("^([a-zA-z,.-]+)$")Required(ErrorMessage = "Invalid Name")]
-        public string Lastname { get; set; }
-
-        [RegularExpression("^([a-zA-z0-9 ,.-]+)$")Required(ErrorMessage = "Invalid Address")]
-        public string Unit { get; set; }
-
-        [RegularExpression("^([a-zA-z0-9 ,.-]+)$")Required(ErrorMessage = "Invalid Address")]
-        public string Street { get; set; }
-
-        [RegularExpression("^([a-zA-z0-9 ,.-]+)$")Required(ErrorMessage = "Invalid Address")]
-        public string Suburb { get; set; }
-
-        [RegularExpression("^([a-zA-z0-9 ,.-]+)$")Required(ErrorMessage = "Invalid Address")]
-        public string State { get; set; }
-
-        [RegularExpression("^([0-9]{4})$")Required(ErrorMessage = "Invalid Address")]
-        public string Postcode { get; set; }
+        public string FullName { get; set; }
 
         [RegularExpression("^([a-z0-9_.-]+[@]{1}[a-z0-9]{1,15}[.]{1}[a-z]{1,5}[.]{0,1}[a-z]{0,5}[.]{0,1}[a-z]{0,5})$")Required(ErrorMessage = "Invalid Email")]
         public string Email { get; set; }
