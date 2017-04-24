47ab5825e049c7fe0d3c14a23907a2e26e73a434
cre8iveallen <pyeinkiller@gmail.com>
2017-04-25 09:13:30 +1000

    Done generating card display, currently working on expiry validation

--- src/CineplexCinemas/Models/CreditCard.cs
+++ src/CineplexCinemas/Models/CreditCard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,19 @@ namespace CineplexCinemas.Models
 {
     public class CreditCard
     {
+        [Display(Name = "Card Number")]
+        public string cardNumber { get; set; }
+
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+
+        [Display(Name = "Expiry Date")]
+        public string expiryDate { get; set; }
+
+        [Display(Name = "CVC")]
+        public string CVC { get; set; }
     }
 }
