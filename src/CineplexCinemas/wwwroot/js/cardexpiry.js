dbeaf137d2b4643ca5c08b17fbfd09fd09206c34
cre8iveallen <pyeinkiller@gmail.com>
2017-04-25 17:41:36 +1000

    a

---
+++ src/CineplexCinemas/wwwroot/js/cardexpiry.js
@@ -0,0 +1,30 @@
+function expiryChecker(){
+	
+	// Get current month and year
+	var date = new Date();
+	var crntMonth = date.getMonth() + 1;
+	var crntYear = date.getFullYear() - 2000;
+	if (crntMonth.length < 2) { crntMonth = "0" + crntMonth; }
+	
+	// Get month and year values from the user
+	var expiryYear = document.getElementById("expiryYear").value;
+	var expiryMonth = document.getElementById("expiryMonth").value;
+
+	// if current year is greater than expiry year, don't submit
+	if (crntYear > expiryYear)
+	{
+		alert("Your credit card is already expired.");
+		return false;
+	}
+	// if expiry year is equal to current year and expiry month is less than current month or greater than 12, don't submit
+	else if (expiryYear == crntYear && (expiryMonth < crntMonth || expiryMonth > 12))
+	{
+		alert("Your credit card is already expired.");
+		return false;
+	}
+	// If valid, submit
+	else
+	{
+		return true;
+	}
+}
